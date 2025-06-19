5094e800d19070bc422d0d01457d9bd96f787a9e
niel2512 <nielyusuf17@gmail.com>
2025-06-19 12:06:00 +0700

    Make a Component Footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <footer class="hidden md:block bg-transparent h-24 w-full fixed bottom-0 left-0">
+        <div class="max-w-7xl mx-auto h-full flex items-center justify-center py-8 px-4 sm:px-6 lg:px-8">
+            <div class="text-slate-800 dark:text-white">
+                Copyright &copy; Zmijewski 2025
+            </div>
+        </div>
+    </footer>
+</div>
